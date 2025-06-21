2267b134b67ecbef0cb982cf68114b5ec92e9714
Samiran <samiran.gode.work@gmail.com>
2025-06-21 19:36:02 +0200

    convolution optimization

--- day_14/padding_stride_shared_memory.cu
+++ day_14/padding_stride_shared_memory.cu
@@ -3,7 +3,7 @@
 
 #define N 4
 #define D 4
-#define TILE 2
+#define TILE 16
 
 __global__ void conv2d_kernel(const float* img,
                               const float* kernel,
@@ -28,10 +28,77 @@ __global__ void conv2d_kernel(const float* img,
 
 }
 
+__global__ void conv2d_shared(const float* img, const float* kernel, float* output,
+                              int H, int W, int K, int P, int S)
+{
+    extern __shared__ float sh_img[]; // dynamically allocated shared memory
+
+    int tx = threadIdx.x; int ty = threadIdx.y;
+    int out_x = blockIdx.x * blockDim.x + tx;
+    int out_y = blockIdx.y * blockDim.y + ty;
+    int outH = (H + 2 * P - K) / S + 1;
+    int outW = (W + 2 * P - K) / S + 1;
+
+    // Calculate shared memory patch coordinates (with halo from the filter)
+    int sh_x = tx;
+    int sh_y = ty;
+    int img_x = out_x * S - P; 
+    int img_y = out_y * S - P;
+    // Load input into shared memory
+    if(img_x >= 0 && img_x < W && img_y >= 0 && img_y < H){
+        sh_img[sh_y * (TILE + K - 1) + sh_x] = img[img_y * W + img_x];
+    }
+    else{
+        sh_img[sh_y * (TILE + K - 1) + sh_x] = 0.0f;
+    }
+    __syncthreads();
+
+    // compute within bounds
+    if(out_x<outW && out_y<outH){
+        float sum = 0.0f;
+        for(int i = 0; i<K; i++){
+            for(int j = 0; j<K; j++){
+                sum += sh_img[(sh_y + i) * (TILE + K - 1) + (sh_x + j)] * kernel[ i * K + j];
+            }
+        }
+        output[out_y * outW + out_x] = sum;
+    }
+
+}
+
+__global__ void conv2d_kernel_pad_stride(
+    const float* img, const float* kernel, float* output,
+    int H, int W, int K, int P, int S
+){
+    int out_x = blockIdx.x * blockDim.x + threadIdx.x;
+    int out_y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int outH = (H + 2 * P - K) / S + 1;
+    int outW = (W + 2 * P - K) / S + 1;
+
+    if(out_x < outW && out_y < outH){
+        float sum = 0.0f;
+        for(int i = 0; i < K; i++){
+            for(int j = 0; j < K; j++){
+                int img_x = out_x * S + j - P;
+                int img_y = out_y * S + j - P;
+
+                if(img_x >= 0 && img_x < W && img_y >= 0 && img_y < H)
+                    sum += img[img_y * W + img_x] * kernel[i * K + j];
+            }
+        }
+        output[out_y * outW + out_x] = sum;
+    }
+
+
+}
+
 
 int main(){
-    int H = 64, W = 64, K = 4;
-    int outH = H-K+1, outW = W-K+1;
+    int H = 64, W = 64, K = 4, P = 2, S = 2;
+    int outH = (H + 2 * P - K) / S + 1;
+    int outW = (W + 2 * P - K) / S + 1;
+ 
 
     float img[H*W];
     float kernel[K*K];
